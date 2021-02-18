3250b5a6a4bd227d58e027883bf5c8145594faf8
Logan Uibel <lbuibel@gmail.com>
2021-02-18 14:04:35 -0700

    added functions and updated package.json

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # NPM Package Project - DGM370
+
+Each function performs a common sales or business calculation. The functions are as follows:
+
+- margin:
+    Calculates the profit margin percentage based on the retail and wholesale price of a product
+
+- markup:
+    Calculate the percentage/difference between the actual cost and the selling price
+
+- profitPercentage: Calculates the difference in the selling price vs the profit
+
+- interest: calculates the total amount accrued based on principle down payment, interest rate percentage and years.
+
+- discountPrice: calculates the sale price of an item based on the retail price and discount percentage.
--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-module.exports = function tiny(string) {
-    if (typeof string !== "string") throw new TypeError("tiny wants a string")
-    return string.replace(/\s/g, "")
+module.exports = function margin(retail, wholesale) {
+    let margin = Math.round((((retail - wholesale) / retail) * 100))
+    return margin
 }
+
+module.exports = function markup(retail, wholesale) {
+    let markup = ((retail - wholesale) / wholesale)*100
+    return markup
+}
+
+module.exports = function interest(principle, rate, years) {
+    let r = rate/100
+    let a = (principle * 1) + (principle * (r * years))
+    Math.round((a + Number.EPSILON) * 100) / 100
+    return a
+}
+
+module.exports = function profitPercentage(retail, wholesale) {
+    let result = (retail / wholesale)
+    Math.round((result + Number.EPSILON) * 100) / 100
+    return result
+}
+
+module.exports = function discountPrice(price, discountPercent) {
+    let d = discountPercent / 100
+    total = price * d
+    return total
+}
+
